b868b4a207c829e89b146378bdaaef33e3e86a74
= <bozhena.shalginskaia@mail.ru>
2025-03-29 19:14:34 +0300

    add husky

--- backend/src/trpc.ts
+++ backend/src/trpc.ts
@@ -8,6 +8,9 @@ const cards = [
   { nick: 'cool-idea-nick-5', name: 'Idea 5', description: 'Description of idea 5...' },
 ]
 
+const x: string = "hello"
+console.log(x)
+
 const trpc = initTRPC.create()
 
 export const trpcRouter = trpc.router({
