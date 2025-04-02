8465fc446e9ea6fd9c1d5473f6043819fc20745f
= <bozhena.shalginskaia@mail.ru>
2025-04-02 20:20:26 +0300

    Create and add lodash

--- backend/src/trpc.ts
+++ backend/src/trpc.ts
@@ -1,23 +1,18 @@
 import { initTRPC } from '@trpc/server'
+import _ from 'lodash'
 
-const cards = [
-  { nick: 'cool-card-nick-1', name: 'Card 1', description: 'Description of card 1...' },
-  { nick: 'cool-card-nick-2', name: 'Card 2', description: 'Description of card 2...' },
-  { nick: 'cool-card-nick-3', name: 'Card 3', description: 'Description of card 3...' },
-  { nick: 'cool-card-nick-4', name: 'Card 4', description: 'Description of card 4...' },
-  { nick: 'cool-card-nick-5', name: 'Card 5', description: 'Description of card 5...' },
-]
-
-// const x: string = 'hello'
-// if (Math.random() + 1) {
-//   console.info(x)
-// }
+const cards = _.times(100, (i) => ({
+  nick: `cool-card-nick-${i}`,
+  name: `Card ${i}`,
+  description: `Description of card ${i}...`,
+  text: _.times(100, (j) => `<p>Text paragrph ${j} of idea ${i}...</p>`).join(''),
+}))
 
 const trpc = initTRPC.create()
 
 export const trpcRouter = trpc.router({
   getCards: trpc.procedure.query(() => {
-    return { cards }
+    return { cards: cards.map((card) => _.pick(card, ['nick', 'name', 'description'])) }
   }),
 })
 
