2a170375d130d42e6f60f582adb68dba81984d10
= <bozhena.shalginskaia@mail.ru>
2025-05-09 19:20:04 +0300

    Eslint on work

--- webapp/src/pages/SignOutPage/index.tsx
+++ webapp/src/pages/SignOutPage/index.tsx
@@ -10,7 +10,7 @@ export const SignOutPage = () => {
   useEffect(() => {
     Cookies.remove('token')
     void trpcUtils.invalidate().then(() => {
-      navigate(getSignInRoute())
+      void navigate(getSignInRoute())
     })
   }, [])
 
